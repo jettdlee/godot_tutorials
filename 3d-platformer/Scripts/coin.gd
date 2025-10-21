7c87a46b1401bf36c8e14243fdc2ac2338a79a57
Jet Lee <jettdlee@gmail.com>
2025-10-21 21:19:32 +0100

    setup coin

---
+++ 3d-platformer/Scripts/coin.gd
@@ -0,0 +1,18 @@
+extends Area3D
+
+@export var rotate_speed : float = 180.0
+@export var bob_height : float = 0.2
+@export var bob_speed : float = 0.5
+
+@onready var start_y_pos : float = global_position.y
+
+func _process(delta: float) -> void:
+	rotation.y += deg_to_rad(rotate_speed) * delta
+	var time = Time.get_unix_time_from_system()
+	var y_pos = (1 + sin(time * bob_speed)) / 2  * bob_height
+	global_position.y = start_y_pos + y_pos
+
+func _on_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("Player"):
+		return
+	queue_free()
