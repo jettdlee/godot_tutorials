fb004746dd57268f45fbd961c4577635ef6cd137
Jet Lee <jettdlee@gmail.com>
2025-12-29 16:54:39 +0000

    Setup shooting

--- roguelike/Scripts/player.gd
+++ roguelike/Scripts/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 50
+@export var shoot_rate : float = 0.4
+var last_shoot_time : float
+var projectile_scene : PackedScene = preload("res://Scenes/Projectiles/projectile.tscn")
 
 @onready var sprite : Sprite2D = $Sprite
 @onready var weapon_origin : Node2D = $Weapon
@@ -17,3 +20,19 @@ func _process(delta: float) -> void:
 	weapon_origin.rotation_degrees = rad_to_deg(mouse_dir.angle()) + 90
 	
 	sprite.flip_h = mouse_dir.x < 0
+	
+	if Input.is_action_pressed("attack"):
+		if Time.get_unix_time_from_system() - last_shoot_time > shoot_rate:
+			_shoot()
+			
+func _shoot():
+	last_shoot_time = Time.get_unix_time_from_system()
+	var projectile = projectile_scene.instantiate()
+	get_tree().root.add_child(projectile)
+	projectile.global_position = muzzle.global_position
+	projectile.rotation = weapon_origin.rotation
+	projectile.owner_character = self
+	
+func take_damage(amount : int):
+	pass
+	
