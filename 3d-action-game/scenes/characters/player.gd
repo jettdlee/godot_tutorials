c9ba4579cf1b68bfbca243ddfb254082cbcd3212
Jet Lee <jettdlee@gmail.com>
2026-04-03 20:30:50 +0100

    setup grass

--- 3d-action-game/scenes/characters/player.gd
+++ 3d-action-game/scenes/characters/player.gd
@@ -21,6 +21,7 @@ func _ready() -> void:
 	hud.setup(health)
 
 func _physics_process(delta: float) -> void:
+	RenderingServer.global_shader_parameter_set("player_position", global_position)
 	move_logic(delta)
 	jump_logic(delta)
 	ability_logic()
